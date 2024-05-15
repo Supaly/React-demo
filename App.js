0438210c2f76bf57e7421da9a18183b9c43fc558
Supaly <supalygarg5@gmail.com>
2024-05-15 21:49:32 +0530

    part 3

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-  ]);
-  
-  //JSX
-  
-  console.log(parent); // object
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
+
+const element = <h1>this is jsx elememt inside another element </h1>
+
+const element2 = <h5> 
+{element}  another jsx Element </h5>
+ const Title = () => <h1>I am using Jsx</h1>
+
+
+ const Heading = () => (
+  <div id="container">
+    <Title />
+    <Title></Title>
+    {Title()}
+    {element2}
+    <h1>inside heading functional component</h1>
+    </div>
+ );
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(<Heading />);
